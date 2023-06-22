2aeb032a0496a8e71a16ce3904ecb8cc46fa2675
iamdeborah-Q <122198373+iamdeborah-Q@users.noreply.github.com>
2023-06-22 01:55:07 -0400

    added intergration

--- integration-test.sh
+++ integration-test.sh
@@ -15,7 +15,7 @@ then
     response=$(curl -s $applicationURL:$PORT$applicationURI)
     http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
 
-    #if [[ "$response" == 100 ]];
+    if [[ "$response" == 100 ]];
         then
             echo "Increment Test Passed"
         else
@@ -23,7 +23,7 @@ then
             exit 1;
     fi;
 
-    if [[ "$http_code" == 200 ]];
+    if [[ "$http_code" == 000 ]];
         then
             echo "HTTP Status Code Test Passed"
         else
